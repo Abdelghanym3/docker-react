4da2cc4978ee3c2dda28fb3571260c836767b9e6
Mohamed AbdelGhany <mohamedabdelghany28@gmail.com>
2020-08-24 04:15:14 +0200

    modify app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           again Hi Hi there, Edit <code>src/App.js</code> and save to reload.
+           new branch again Hi Hi there, Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
